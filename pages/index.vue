b7c3ff9b42fccc0cf73cd3a6cceaa4dd483dbb17
vanjasoa <99241852+vanjapro@users.noreply.github.com>
2022-12-07 08:52:40 +0300

    test modif

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
   <main>
     <section class="hero-section">
       <header>
-        <h1 class="font-black text-8xl">Welcome to my site</h1>
+        <h1 class="font-black text-8xl">Welcome to Liberty lady</h1>
         <p>Take a look at <NuxtLink to="/blog">my blog</NuxtLink></p>
       </header>
     </section>
